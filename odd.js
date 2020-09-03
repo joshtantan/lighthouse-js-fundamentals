ae699072321f7e10b0fdb0dfaf8fe85749b8b541
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-03 20:34:50 +0000

    Implement isOdd function

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(n) {
+  if (n <= 0 || n % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
